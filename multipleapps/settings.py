4e6ce56e35e34429601307718191d42b10d9e121
rjalmeda <rjalmeda@gmail.com>
2016-09-21 15:20:29 -0700

    yay

--- multipleapps/settings.py
+++ multipleapps/settings.py
@@ -78,16 +78,10 @@ WSGI_APPLICATION = 'multipleapps.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'multiapps',
-        'USER': 'rjalmeda',
-        'PASSWORD': 'rjalmeda',
-        'HOST': 'localhost',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
 }
 
 
